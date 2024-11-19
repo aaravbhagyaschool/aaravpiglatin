9246c09d55333d5c637b276ac3bb613e6489f8e5
aaravbhagyaschool <ab7857@pleasantonusd.net>
2024-11-19 22:07:10 +0000

    new changes in PigLatinTranslator

--- src/PigLatinTranslator.java
+++ src/PigLatinTranslator.java
@@ -1,61 +1,98 @@
-import java.lang.*;
+public class PigLatinTranslator {
+  public static Book translate(Book input) {
+      Book translatedBook = new Book();
 
-public class PigLatinTranslator
-{
-  public static Book translate(Book input)
-  {
-    Book translatedBook = new Book();
+      for (int i = 0; i < input.getLineCount(); i++) {
+          String line = input.getLine(i);
+          translatedBook.appendLine(translate(line));
+      }
 
-    // Add code here to populate translatedBook with a translation of the input book.
-    // Curent do-nothing code will return an empty book.
-
-    return translatedBook;
+      return translatedBook;
   }
 
-  public static String translate(String input)
-  {
+  public static String translate(String input) {
+      if (input.trim().isEmpty()) {
+          return input;
+      }
 
-     String a = "a";
-     String e = "e";
-     String i = "i";
-     String o = "o";
-     String u = "u";
-  // YOU HAVE TO MAKE THE VOWEL BE THE FIRST LETTER OF THE WORD
-     if (input.indexOf(a) == 1)
-     {
-   // String expected = expected.substring(1,expected.length()); 
-     }
-     else if (input.indexOf(e) == 1)
-     {
-     
-     }
-     else if (input.indexOf(i) == 1)
-     {
-     
-     }
-     else if (input.indexOf(o) == 1)
-     {
-     
-     }
-     else if (input.indexOf(u) == 1)
-     {
-     
-     }
-    String result = translateWord(input);
+      StringBuilder result = new StringBuilder();
+      String[] words = input.split("\\s+");
 
-    return result;
+      for (String word : words) {
+          if (!word.isEmpty()) {
+              result.append(translateWord(word)).append(" ");
+          }
+      }
+
+      return result.toString().trim();
   }
 
-  private static String translateWord(String input)
-  {
-   
- String result = input;
-    
-    return result;
+  private static String translateWord(String input) {
+      String originalWord = input;
+      boolean isCapitalized = Character.isUpperCase(input.charAt(0));
+      boolean isMixedCapitalization = !input.equals(input.toLowerCase()) && !input.equals(input.toUpperCase());
+      String prefix = "";
+      String suffix = "";
+      int hyphenIndex = input.indexOf('-');
+      if (hyphenIndex > 0) {
+          String firstPart = translateWord(input.substring(0, hyphenIndex));
+          String secondPart = translateWord(input.substring(hyphenIndex + 1));
+          return firstPart + "-" + secondPart;
+      }
+
+      if (input.matches(".*\\p{Punct}$")) {
+          suffix = input.substring(input.length() - 1);
+          input = input.substring(0, input.length() - 1);
+      }
+
+      // Translate core word
+      String coreWord = input.toLowerCase();
+      if (startsWithVowel(coreWord)) {
+          coreWord = coreWord + "ay";
+      } else {
+          int firstVowelIndex = findFirstVowelIndex(coreWord);
+          if (firstVowelIndex > 0) {
+              coreWord = coreWord.substring(firstVowelIndex) + coreWord.substring(0, firstVowelIndex) + "ay";
+          }
+      }
+      if (isCapitalized) {
+          coreWord = capitalizeFirstLetter(coreWord);
+      } else if (isMixedCapitalization) {
+          coreWord = preserveMixedCapitalization(coreWord, originalWord);
+      }
+
+      return prefix + coreWord + suffix;
   }
 
-  // Add additonal private methods here.
-  // For example, I had one like this:
-  // private static String capitalizeFirstLetter(String input)
+  private static boolean startsWithVowel(String word) {
+      return word.matches("^[aeiouAEIOU].*");
+  }
 
+  private static int findFirstVowelIndex(String word) {
+      for (int i = 0; i < word.length(); i++) {
+          if ("aeiou".indexOf(word.charAt(i)) >= 0) {
+              return i;
+          }
+      }
+      return -1;
+  }
+
+  private static String capitalizeFirstLetter(String input) {
+      if (input.isEmpty()) {
+          return input;
+      }
+      return input.substring(0, 1).toUpperCase() + input.substring(1);
+  }
+
+  private static String preserveMixedCapitalization(String transformed, String original) {
+      StringBuilder result = new StringBuilder();
+      for (int i = 0; i < original.length(); i++) {
+          if (i < transformed.length()) {
+              result.append(Character.isUpperCase(original.charAt(i))
+                      ? Character.toUpperCase(transformed.charAt(i))
+                      : transformed.charAt(i));
+          }
+      }
+      return result.toString();
+  }
 }
